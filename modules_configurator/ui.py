598481655b1367e2d10c702d849caf752675ce2c
J-KOEL <jaysonkoel05@gmail.com>
2025-08-11 14:02:34 -0500

    Update ui.py

--- modules_configurator/ui.py
+++ modules_configurator/ui.py
@@ -4,16 +4,19 @@ def render_dropdown(label, options_dict):
     return st.selectbox(label, list(options_dict.keys()))
 
 def render_product_selector():
-    return st.selectbox("Select Product Type", [
-        "Non-Illuminated Pushbuttons",
-        "Non-Illuminated Pushpulls",
-        "Illuminated Incandescent Pushpulls",
-        "Illuminated LED Pushpulls",
-        "Illuminated Incandescent Pushbuttons",
-        "Illuminated LED Pushbuttons"
-        "Standard Indicating Lights - Incandescent"
-        "Standard Indicating Lights - LED"
-        "PresTest Incandescent"
-        "PresTest LED"
-        "Master Test Incandescent"
-    ])
+    return st.selectbox(
+        "Select Product Type",
+        [
+            "Non-Illuminated Pushbuttons",
+            "Non-Illuminated Pushpulls",
+            "Illuminated Incandescent Pushpulls",
+            "Illuminated LED Pushpulls",
+            "Illuminated Incandescent Pushbuttons",
+            "Illuminated LED Pushbuttons",
+            "Standard Indicating Lights - Incandescent",
+            "Standard Indicating Lights - LED",
+            "PresTest Incandescent",
+            "PresTest LED",
+            "Master Test Incandescent",
+        ],
+    )
