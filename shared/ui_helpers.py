4a1050f5951d27600a9adbfe1833cd0ac3056675
J-KOEL <jaysonkoel05@gmail.com>
2025-08-11 14:00:59 -0500

    Update ui_helpers.py

--- shared/ui_helpers.py
+++ shared/ui_helpers.py
@@ -1,1 +1,25 @@
+import streamlit as st
+
+def render_dropdown(label, options_dict):
+    """Renders a selectbox returning the chosen *label* key."""
+    return st.selectbox(label, list(options_dict.keys()))
+
+def render_product_selector():
+    """Fixed version of your original list with missing commas + consistent labels."""
+    return st.selectbox(
+        "Select Product Type",
+        [
+            "Non-Illuminated Pushbuttons",
+            "Non-Illuminated Pushpulls",
+            "Illuminated Incandescent Pushpulls",
+            "Illuminated LED Pushpulls",
+            "Illuminated Incandescent Pushbuttons",
+            "Illuminated LED Pushbuttons",
+            "Standard Indicating Lights - Incandescent",
+            "Standard Indicating Lights - LED",
+            "PresTest Incandescent",
+            "PresTest LED",
+            "Master Test Incandescent",
+        ],
+    )
 
