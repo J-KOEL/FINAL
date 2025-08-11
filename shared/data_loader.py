3e799cea200242c4e0d93f0bdac1f00414fe592a
J-KOEL <jaysonkoel05@gmail.com>
2025-08-11 14:00:49 -0500

    Update data_loader.py

--- shared/data_loader.py
+++ shared/data_loader.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def load_mapping(file_path):
+    df = pd.read_csv(file_path)
+    df.columns = df.columns.str.strip()
+    df = df.dropna(how='all', axis=1)
+    df = df.dropna(how='all')
+    return dict(zip(df['Label'].str.strip(), df['Code'].astype(str).str.strip()))
 
